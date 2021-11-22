c781fa9cd1b8120c6629bd55aa7e77b1f6f14700
Joy <joyderuijter@gmail.com>
2021-11-22 19:06:41 +0100

    Add weapon switching

---
+++ Assets/Scripts/WeaponSwitching.cs
@@ -0,0 +1,72 @@
+using UnityEngine;
+
+public class WeaponSwitching : MonoBehaviour
+{
+    public int selectedWeapon = 0;
+
+    void Start()
+    {
+        SelectWeapon();
+    }
+
+    void Update()
+    {
+        int previousSelectedWeapon = selectedWeapon;
+
+        HandleInput();
+
+        if (previousSelectedWeapon != selectedWeapon)
+        {
+            SelectWeapon();
+        }
+    }
+
+    private void SelectWeapon()
+    {
+        int i = 0;
+        foreach (Transform weapon in transform)
+        {
+            if (i == selectedWeapon)
+            {
+                weapon.gameObject.SetActive(true);
+            }
+            else
+            {
+                weapon.gameObject.SetActive(false);
+            }
+            i++;
+        }
+    }
+
+    private void HandleInput()
+    {
+        // Scrollwheel
+        if (Input.GetAxis("Mouse ScrollWheel") > 0f) // Up
+        {
+            if (selectedWeapon >= transform.childCount - 1)
+                selectedWeapon = 0;
+            else
+                selectedWeapon++;
+        }
+        if (Input.GetAxis("Mouse ScrollWheel") < 0f) // Down
+        {
+            if (selectedWeapon <= 0)
+                selectedWeapon = transform.childCount - 1;
+            else
+                selectedWeapon--;
+        }
+
+        // Numberkeys
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+            selectedWeapon = 0;
+
+        if (Input.GetKeyDown(KeyCode.Alpha2) && transform.childCount >= 2)
+            selectedWeapon = 1;
+
+        if (Input.GetKeyDown(KeyCode.Alpha3) && transform.childCount >= 3)
+            selectedWeapon = 2;
+
+        if (Input.GetKeyDown(KeyCode.Alpha4) && transform.childCount >= 4)
+            selectedWeapon = 3;
+    }
+}
