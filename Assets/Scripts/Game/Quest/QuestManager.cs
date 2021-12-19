8f6c898a13727f3aa01afba07cac33e357c533bc
Joy <joyderuijter@gmail.com>
2021-12-19 21:03:42 +0100

    Clean up the QuestManager script

--- Assets/Scripts/Game/Quest/QuestManager.cs
+++ Assets/Scripts/Game/Quest/QuestManager.cs
@@ -6,56 +6,70 @@ using UnityEngine.UI;
 
 public class QuestManager : MonoBehaviour
 {
-    [SerializeField] private GameObject questPrintBox, buttonPrefab, finalPopup, player;
-    [SerializeField] private GameObject A, B, C, D, E;
-    private QuestEvent final;
-    public Quest quest = new Quest();
-
-    public List<BaseQuest> quests;
+    #region Variables
+    [Header("Gameobject references")]
+    [SerializeField] private GameObject questPrintBox;
+    [SerializeField] private GameObject buttonPrefab;
+    [SerializeField] private GameObject finalPopup;
+    [SerializeField] private GameObject player;
     public Dictionary<string, BaseQuest> questDictionary = new Dictionary<string, BaseQuest>();
 
-    void Start()
+    [Header("List of quests")]
+    [Space(10)]
+    public List<BaseQuest> quests;
+
+    public Quest quest = new Quest();
+    private QuestEvent final;
+    #endregion
+
+    void Awake()
+    {
+        DefineQuestEvents();
+    }
+
+    private void DefineQuestEvents()
     {
         List<QuestEvent> questEvents = new List<QuestEvent>();
+        List<GameObject> questObjects = new List<GameObject>();
 
+        //Add quests to dictionary, create the questevents, add questevents and questobjects to a list
         foreach (var item in quests)
         {
             questDictionary.Add(item.questName, item);
             QuestEvent questEvent = quest.AddQuestEvent(item.questName, item.questDescription, item.gameObject);
             questEvents.Add(questEvent);
+            questObjects.Add(item.gameObject);
         }
 
-        //define the paths between the events - e.g. the order they must be completed
+        //Define the paths between the events
         quest.AddPath(questEvents[0].GetId(), questEvents[1].GetId());
         quest.AddPath(questEvents[0].GetId(), questEvents[2].GetId());
         quest.AddPath(questEvents[1].GetId(), questEvents[2].GetId());
         quest.AddPath(questEvents[2].GetId(), questEvents[3].GetId());
         quest.AddPath(questEvents[3].GetId(), questEvents[4].GetId());
 
+        //Run breadth first search
         quest.BFS(questEvents[0].GetId());
 
+        //Create the quest buttons for the events
         QuestButton button = CreateButton(questEvents[0]).GetComponent<QuestButton>();
-        A.GetComponent<BaseQuest>().Setup(this, questEvents[0], button);
+        questObjects[0].GetComponent<BaseQuest>().Setup(this, questEvents[0], button);
         button = CreateButton(questEvents[1]).GetComponent<QuestButton>();
-        B.GetComponent<QuestLocation>().Setup(this, questEvents[1], button);
+        questObjects[1].GetComponent<BaseQuest>().Setup(this, questEvents[1], button);
         button = CreateButton(questEvents[2]).GetComponent<QuestButton>();
-        C.GetComponent<QuestKill>().Setup(this, questEvents[2], button);
+        questObjects[2].GetComponent<BaseQuest>().Setup(this, questEvents[2], button);
         button = CreateButton(questEvents[3]).GetComponent<QuestButton>();
-        D.GetComponent<QuestPickup>().Setup(this, questEvents[3], button);
+        questObjects[3].GetComponent<BaseQuest>().Setup(this, questEvents[3], button);
         button = CreateButton(questEvents[4]).GetComponent<QuestButton>();
-        E.GetComponent<QuestLocation>().Setup(this, questEvents[4], button);
+        questObjects[4].GetComponent<BaseQuest>().Setup(this, questEvents[4], button);
 
-        final = questEvents[4];
-
-        //BaseQuest quest = A.GetComponent<BaseQuest>();
-        if (quest.GetType() == typeof(QuestLocation))
-        { 
-            
-        }
+        //Save the final quest
+        final = questEvents[questEvents.Count - 1];
     }
 
     GameObject CreateButton(QuestEvent e)
     {
+        //Instantiate the buttons and update their status
         GameObject b = Instantiate(buttonPrefab);
         b.GetComponent<QuestButton>().Setup(e, questPrintBox);
         if (e.order == 1)
@@ -68,9 +82,10 @@ public class QuestManager : MonoBehaviour
 
     public void UpdateQuestOnCompletion(QuestEvent e)
     {
+        //Execute some actions if the last quest is completed
         if (e == final)
         {
-            StartCoroutine(DisplayVictoryPopup(3));
+            StartCoroutine(DisplayPopup(3));
             player.GetComponent<Player>().currentMoney += 500;
             player.GetComponent<Player>().currentXp += 100;
             return;
@@ -78,17 +93,18 @@ public class QuestManager : MonoBehaviour
 
         foreach (QuestEvent n in quest.questEvents)
         {
-            //if this event is next in order
+            //If this event is next in order
             if (n.order == (e.order + 1))
             {
-                //make the next in line available for completion
+                //Make it available for completion
                 n.UpdateQuestEvent(QuestEvent.EventStatus.CURRENT);
             }
         }
     }
 
-    private IEnumerator DisplayVictoryPopup(float seconds)
+    private IEnumerator DisplayPopup(float seconds)
     {
+        //Display the final popup and deactivate it after seconds
         finalPopup.SetActive(true);
         yield return new WaitForSeconds(seconds);
         finalPopup.SetActive(false);
